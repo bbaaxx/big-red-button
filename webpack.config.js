05240981794004cdfda579fe049a557e68643ad2
Eduardo M (bbaaxx) <bbaaxx@gmail.com>
2017-06-06 16:38:42 -0500

    added lots of stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,84 @@
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackManifestPlugin = require('webpack-manifest-plugin');
+const FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
+
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 
 const baseConfig = {
-    entry: {
-      // libs: './libs.js',
-      main: './index.js'
-    },
-    output: {
-        filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.(js|jsx)$/,
-                exclude: path.resolve(__dirname, "node_modules"),
-                use: [
-                  {
-                    loader: 'eslint-loader',
-                    options: {
-                      failOnError: true
-                    }
-                  }
-                ]
+  entry: {
+    // libs: './libs.js',
+    main: './index.js',
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: nodeModulesPath,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              failOnError: true,
             },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: path.resolve(__dirname, "node_modules"),
-                use: [
-                  {loader: 'babel-loader'},
-                ]
-            }
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: nodeModulesPath,
+        use: [
+          { loader: 'babel-loader' },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
         ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin(),
-      new WebpackManifestPlugin(),
-      // why can't I use arrows ?
-      function() {
-        // must reference this ?
-        this.plugin('done', (stats) => {
-          console.log('weehee');
-        });
-      }
+      },
     ],
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      overlay: true,
-      port: 3010
+  },
+  plugins: [
+    new FlowBabelWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+    new WebpackManifestPlugin(),
+    // why can't I use arrows ?
+    function customPlugin() {
+      // must reference this ?
+      this.plugin('done', () => {
+        console.log('weehee');
+      });
     },
-    devtool: 'cheap-eval-source-map'
-}
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    overlay: true,
+    port: 3010,
+  },
+  devtool: 'cheap-eval-source-map',
+};
 
-let targets = [
+const targets = [
   // 'web', 'webworker', 'node', 'async-node', 'node-webkit', 'electron-main'
-  'web'
+  'web',
 ].map((target) => {
-  let base = webpackMerge(baseConfig, {
-    target: target,
+  const base = webpackMerge(baseConfig, {
+    target,
     output: {
-      path: path.resolve(__dirname, 'dist/' + target),
-      filename: '[name].' + target + '.js'
-    }
+      path: path.resolve(`${__dirname}dist/${target}`),
+      filename: `[name].${target}.js`,
+    },
   });
   return base;
 });
